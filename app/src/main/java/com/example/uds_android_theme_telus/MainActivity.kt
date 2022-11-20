0349a09b3e07106fa04b448a512943055b3d394e
QuickChangeSpidey <akshay.pandey@telus.com>
2022-11-19 21:56:22 -0500

    test theme added again

--- app/src/main/java/com/example/uds_android_theme_telus/MainActivity.kt
+++ app/src/main/java/com/example/uds_android_theme_telus/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.uds_android_theme_telus
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         println(Sydney.suburbs)
+        val assetManager = assets
+        assetManager.open("telusTheme.json")
     }
 }
