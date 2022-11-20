809ec65106ce42f76a3c3a42b872ef5934fe0fa9
QuickChangeSpidey <akshay.pandey@telus.com>
2022-11-20 01:24:00 -0500

    file placement test

--- app/src/main/java/com/example/uds_android_theme_telus/MainActivity.kt
+++ app/src/main/java/com/example/uds_android_theme_telus/MainActivity.kt
@@ -2,13 +2,14 @@ package com.example.uds_android_theme_telus
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import java.nio.file.AccessMode
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         println(Sydney.suburbs)
-        val assetManager = assets
-        assetManager.open("telusTheme.json")
+        object {}.javaClass.getResourceAsStream("/raw/telus_theme.json")?.use { it.reader(Charsets.UTF_8).readText() }
+
     }
 }
