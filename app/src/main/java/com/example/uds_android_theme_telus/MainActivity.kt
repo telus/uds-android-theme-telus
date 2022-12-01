807169fa8dbc23c26170d154c494c87bb5170668
QuickChangeSpidey <akshay.pandey@telus.com>
2022-12-01 12:44:57 -0500

    better implementation

--- app/src/main/java/com/example/uds_android_theme_telus/MainActivity.kt
+++ app/src/main/java/com/example/uds_android_theme_telus/MainActivity.kt
@@ -2,13 +2,12 @@ package com.example.uds_android_theme_telus
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import java.nio.file.AccessMode
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val test = resources.openRawResource(com.telus.uds_android_theme_telus.R.raw.telus_theme).use { it.reader(Charsets.UTF_8).readText() }
-        println(test)
+        val test = resources.get()
+        print(test)
     }
 }
--- uds-android-theme-telus/src/main/java/com/example/uds_android_theme_telus/ResourceUtil.kt
+++ uds-android-theme-telus/src/main/java/com/example/uds_android_theme_telus/ResourceUtil.kt
@@ -1,9 +1,10 @@
 package com.example.uds_android_theme_telus
 
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
+import com.telus.uds_android_theme_telus.R
 
-class ResourceUtil: AppCompatActivity() {
-    fun get() {
-        return resources.openRawResource(com.telus.uds_android_theme_telus.R.raw.telus_theme).use { it.reader(Charsets.UTF_8).readText() }
+     fun Resources.get(): String {
+        return openRawResource(R.raw.telus_theme).use { it.reader(Charsets.UTF_8).readText() }
     }
-}
+
